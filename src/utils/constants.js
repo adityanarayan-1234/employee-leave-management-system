fc94404a449954fe2971cd7b8c2d2ea128b2b8b0
Aditya Narayan <your_email@example.com>
2026-07-22 20:12:46 +0530

    Updated API URL for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://employee-leave-management-api-1.onrender.com";
 
 export const ROLES = {
   ADMIN: "admin",
